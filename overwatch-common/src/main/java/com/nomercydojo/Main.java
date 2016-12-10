11defd3944dc8a8f741b719c78cc4f94167e47c4
Tim D <timothy.driscoll@gmail.com>
2016-12-09 23:38:20 -0700

    more setup

--- overwatch-common/src/main/java/com/nomercydojo/Main.java
+++ overwatch-common/src/main/java/com/nomercydojo/Main.java
@@ -1,4 +1,12 @@
 package com.nomercydojo;
 
+
+import org.apache.http.client.methods.HttpGet;
+
 public class Main {
+  private static final String OW_URL_FMT = "https://playoverwatch.com/en-us/career/pc/en-us/%s";
+
+  HttpGet get = new HttpGet(String.format(OW_URL_FMT, "liquidswords-11931"));
+
+
 }
